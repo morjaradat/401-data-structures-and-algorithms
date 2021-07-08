8a157f1b6aac48c761779fff6da963ddb018418b
Mohammad jaradat <morjaradat66@gmail.com>
2021-07-08 22:00:53 +0300

    add new data structure  stack and queues

--- challenge/array-reverse/Main.java
+++ challenge/array-reverse/Main.java
@@ -1,18 +1,18 @@
+import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args) {
-    int[] arr = { 1, 2, 3, 4, 5 };
-    int[] revArr = new int[arr.length];
-    for (int i = 0; i < arr.length; i++) {
-      int j = arr.length - 1 - i;
-      int y = arr[j];
+    int[] array = { 1, 2, 3, 4, 5 };
+    System.out.println(Arrays.toString(arrayReverse(array)));
+  }
+
+  public static int[] arrayReverse(int[] array) {
+    int[] revArr = new int[array.length];
+    for (int i = 0; i < array.length; i++) {
+      int j = array.length - 1 - i;
+      int y = array[j];
       revArr[i] = y;
     }
-    System.out.println("---------");
-    System.out.println(revArr[0]);
-    System.out.println(revArr[1]);
-    System.out.println(revArr[2]);
-    System.out.println(revArr[3]);
-    System.out.println(revArr[4]);
+    return revArr;
   }
 }
